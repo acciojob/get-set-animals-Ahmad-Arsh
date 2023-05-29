e6b55a969e971ba736a570572396b8e89e11e551
Arsh Ahmad <123231460+Ahmad-Arsh@users.noreply.github.com>
2023-05-29 13:48:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
+
 class Animal {
   constructor(species) {
-    this._species = species;
+    this.species = species;
   }
 
-  get species() {
-    return this._species;
+  getSpecies() {
+    return this.species;
   }
 
   makeSound() {
     console.log("The animal makes a sound.");
-  }
-}
-
-class Dog extends Animal {
-  constructor(species) {
-    super(species);
-  }
-
-  bark() {
-    console.log("woof");
   }
 }
 
@@ -32,6 +23,15 @@ class Cat extends Animal {
   }
 }
 
+class Dog extends Animal {
+  constructor(species) {
+    super(species);
+  }
+
+  bark() {
+    console.log("woof");
+  }
+}
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
